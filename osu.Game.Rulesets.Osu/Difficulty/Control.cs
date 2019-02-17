f6837823f670f65bc9bc8acfee4986f3108812c8
MBmasher <38526268+MBmasher@users.noreply.github.com>
2019-02-17 12:20:14 +1100

    change sinusoid function and move it to skill.cs

---
+++ osu.Game.Rulesets.Osu/Difficulty/Control.cs
@@ -0,0 +1,161 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.IO;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Osu.Objects;
+
+namespace osu.Game.Rulesets.Osu.Difficulty.Skills
+{
+    /// <summary>
+    /// Represents the skill required to stay in control of your movement and tapping.
+    /// </summary>
+    public class Control : Skill
+    {
+        protected override double SkillMultiplier => 5000;
+        protected override double StrainDecayBase => 0.35;
+        private const double time_scale_factor = 50;
+        private const double pattern_variety_scale = 2.0;
+        private const double time_variety_scale = 0.8;
+        protected override double StrainValueOf(OsuDifficultyHitObject current)
+        {
+            double calculateDistance(OsuDifficultyHitObject obj) => obj.JumpDistance + obj.TravelDistance;
+
+            if (Previous.Count > 1) 
+            {
+                // Cursor velocity calc
+                double currCursorVel = calculateDistance(current) / current.DeltaTime;
+                double prevCursorVel = calculateDistance(Previous[0]) / Previous[0].DeltaTime;
+                double prevprevCursorVel = calculateDistance(Previous[1]) / Previous[1].DeltaTime;
+
+                double diffVel = Math.Abs(currCursorVel - prevCursorVel);
+                double prevDiffVel = Math.Abs(prevCursorVel - prevprevCursorVel);
+
+                double avgVel = (currCursorVel + prevCursorVel) / 2.0;
+                double prevAvgVel = (prevprevCursorVel + prevCursorVel) / 2.0;
+
+                double currVelChange = avgVel != 0 ? diffVel / avgVel : 0;
+                double prevVelChange = prevAvgVel != 0 ? prevDiffVel / prevAvgVel : 0;
+                double totalVelChange = currVelChange * prevVelChange;
+
+                // Distance calc
+                double distDiff = Math.Abs(calculateDistance(current) - calculateDistance(Previous[0]));
+                double prevDistDiff = Math.Abs(calculateDistance(Previous[0]) - calculateDistance(Previous[1]));
+
+                double avgDist = (calculateDistance(current) + calculateDistance(Previous[0])) / 2.0;
+                double prevAvgDist = (calculateDistance(Previous[0]) + calculateDistance(Previous[1])) / 2.0;
+
+                double currDistChange = avgDist != 0 ? distDiff / avgDist : 0;
+                double prevDistChange = prevAvgDist != 0 ? prevDistDiff / prevAvgDist : 0;
+                double totalDistChange = currDistChange * prevDistChange;
+
+                // Angle calc
+                double angleScale = 0;
+                double angleStdDev = 0;
+
+                if (current.Angle != null && Previous[0].Angle != null)
+                {
+                    double maxStdDev;
+                    if (Previous[1].Angle != null)
+                    {
+                        double angleAvg = (current.Angle.Value + Previous[0].Angle.Value + Previous[1].Angle.Value) / 3.0;
+                        angleStdDev = Math.Sqrt((Math.Pow(current.Angle.Value - angleAvg, 2.0) + Math.Pow(Previous[0].Angle.Value - angleAvg, 2.0) + Math.Pow(Previous[1].Angle.Value - angleAvg, 2.0)) / 2.0);
+                        maxStdDev = Math.Sqrt((2 * Math.Pow(Math.PI / 3.0, 2.0) + Math.Pow(Math.PI - Math.PI / 3.0, 2.0)) / 2.0);
+                    } else
+                    {
+                        double angleAvg = (current.Angle.Value + Previous[0].Angle.Value) / 2.0;
+                        angleStdDev = Math.Sqrt(Math.Pow(current.Angle.Value - angleAvg, 2.0) + Math.Pow(Previous[0].Angle.Value - angleAvg, 2.0));
+                        maxStdDev = Math.Sqrt(2 * Math.Pow(Math.PI / 3.0, 2.0) + Math.Pow(Math.PI - Math.PI / 3.0, 2.0));
+                    }
+                    angleScale = Math.Pow(angleStdDev / maxStdDev, 2.0);
+                }
+
+                // Slider calc
+                double sliderChange = 0;
+
+                if (current.BaseObject is Slider currentSlider && Previous[0].BaseObject is Slider previousSlider)
+                {
+                    double sliderDiff = Math.Abs(currentSlider.Velocity - previousSlider.Velocity);
+                    double sliderAvg = (currentSlider.Velocity + previousSlider.Velocity) / 2.0;
+                    sliderChange = sliderAvg != 0 ? 1.5 * sliderDiff / sliderAvg : 0;
+                }
+
+                // Time calc
+                double timeDiff = Math.Abs(current.DeltaTime - Previous[0].DeltaTime);
+                double prevtimeDiff = Math.Abs(Previous[0].DeltaTime - Previous[1].DeltaTime);
+
+                double timeAvg = (current.DeltaTime + Previous[0].DeltaTime) / 2.0;
+                double prevTimeAvg = (Previous[0].DeltaTime + Previous[1].DeltaTime) / 2.0;
+
+                double currTimeChange = timeAvg != 0 ? timeDiff / timeAvg : 0;
+                double prevTimeChange = prevTimeAvg != 0 ? prevtimeDiff / prevTimeAvg : 0;
+
+                // Unorthodox rhythm gives higher values
+                currTimeChange = sinusoid(currTimeChange);
+                prevTimeChange = sinusoid(prevTimeChange);
+				
+                double totalTimeChange = Math.Max(currTimeChange, prevTimeChange);
+
+                // Apply dec. multipliers to values for non-constant rhythm/stacks
+                double stackScale = Math.Min(1.0, Math.Min(Math.Pow((calculateDistance(current)) / 100, 2.0), Math.Pow((calculateDistance(Previous[0])) / 100, 2.0)));
+                double timeScale = time_scale_factor / (time_scale_factor + (timeDiff + prevtimeDiff) / 2.0);
+
+                // Final values
+                double patternResult = stackScale * pattern_variety_scale * timeScale * angleScale * Math.Pow(totalVelChange * totalDistChange, 0.5);
+                double timeResult = Math.Max(timeMultiplier(current), timeMultiplier(Previous[0])) * Math.Pow(totalTimeChange, time_variety_scale);
+
+                /*Console.WriteLine("---");
+                Console.WriteLine("Object placed: " + current.BaseObject.StartTime);
+                Console.WriteLine("timeDiff: " + timeDiff);
+                Console.WriteLine("prevtimeDiff: "+ prevtimeDiff);
+                Console.WriteLine("timeAvg: " + timeAvg);
+                Console.WriteLine("prevTimeAvg: "+ prevTimeAvg);
+                Console.WriteLine("currTimeChange: " + currTimeChange);
+                Console.WriteLine("prevTimeChange: "+ prevTimeChange);
+                Console.WriteLine("totalTimeChange: " + totalTimeChange);
+                Console.WriteLine("stackScale: " + stackScale);
+                Console.WriteLine("pattern_variety_scale: " + pattern_variety_scale);
+                Console.WriteLine("timeScale: " + timeScale);
+                Console.WriteLine("angleScale: " + angleScale);
+                Console.WriteLine("totalVelChange: " + totalVelChange);
+                Console.WriteLine("totalDistChange: " + totalDistChange);
+                Console.WriteLine("patternResult: " + patternResult);
+                Console.WriteLine("timeResult: " + timeResult);*/
+
+                /*var document = new Document(
+                    new Span("---"),
+                    '\n',
+                    new Span("Object placed".PadRight(15) + $": {current.BaseObject.StartTime}"),
+                    '\n',
+                    new Span("Cursor Velocity Change".PadRight(15) + $": {totalVelChange}"),
+                    '\n',
+                    new Span("Distance Change".PadRight(15) + $": {totalDistChange}"),
+                    '\n',
+                    new Span("Angle Change".PadRight(15) + $": {angleStdDev}"),
+                    '\n',
+                    new Span("Time Change".PadRight(15) + $": {totalTimeChange}"),
+                    '\n',
+                    new Span("Slider Change".PadRight(15) + $": {sliderChange}"),
+                    '\n',
+                    new Span("Total Control value".PadRight(15) + $": {timeMultiplier(current) * (stackMultiplier * Math.Pow(totalVelChange * totalDistChange * angleStdDev, 0.25)) / Math.Min(current.StrainTime, Previous[0].StrainTime) + Math.Sqrt(totalTimeChange) / Math.Max(current.StrainTime, Previous[0].StrainTime) + sliderChange / Math.Min(current.StrainTime, Previous[0].StrainTime)}"),
+                    '\n',
+                    '\n'
+                );
+                using (var writer = new StringWriter())
+                {
+                    ConsoleRenderer.RenderDocumentToText(document, new TextRenderTarget(writer));
+                    var str = writer.GetStringBuilder().ToString();
+                    var lines = str.Split('\n');
+                    for (int i = 0; i < lines.Length; i++)
+                        lines[i] = lines[i].TrimEnd();
+                    str = string.Join('\n'.ToString(), lines);
+                    Console.Write(str);
+                    File.AppendAllText(@"A:\Users\oykxf\Documents\osu-tools\objects.txt", str);
+                }*/
+
+                return (timeMultiplier(current) * patternResult + timeResult + sliderChange) / Math.Min(current.StrainTime, Previous[0].StrainTime);            
+            } else return 0;
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Osu/Difficulty/Skill.cs
@@ -0,0 +1,138 @@
+using System.Runtime.Serialization;
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Osu.Difficulty.Utils;
+using osu.Game.Rulesets.Osu.Objects;
+
+namespace osu.Game.Rulesets.Osu.Difficulty.Skills
+{
+    /// <summary>
+    /// Used to processes strain values of <see cref="OsuDifficultyHitObject"/>s, keep track of strain levels caused by the processed objects
+    /// and to calculate a final difficulty value representing the difficulty of hitting all the processed objects.
+    /// </summary>
+    public abstract class Skill
+    {
+        protected const double SINGLE_SPACING_THRESHOLD = 125;
+        protected const double STREAM_SPACING_THRESHOLD = 110;
+        
+        public double timeMultiplier(OsuDifficultyHitObject current) => 1.0 - Math.Tanh((current.StrainTime - 150.0) / 100.0);
+
+		public double sinusoid(double inputNumber)
+        {
+            double ePower, piMultiplier, offset;
+            double changeValue = 2.0 / 3.0;
+            if (inputNumber < changeValue)
+            {
+                ePower = -1.5;
+                piMultiplier = 4.0;
+                offset = -2.0;
+            } else
+            {
+                ePower = -4.0 / 3.0;
+                piMultiplier = 3.0;
+                offset = -2.0 - 1.0 / 6.0;
+            }
+
+            double outputValue = 2.0 * Math.Exp(ePower * inputNumber) * Math.Pow(Math.Sin((piMultiplier * Math.PI) / (inputNumber + offset)), 2.0);
+
+            return outputValue;
+        }
+		
+        /// <summary>
+        /// Strain values are multiplied by this number for the given skill. Used to balance the value of different skills between each other.
+        /// </summary>
+        protected abstract double SkillMultiplier { get; }
+
+        /// <summary>
+        /// Determines how quickly strain decays for the given skill.
+        /// For example a value of 0.15 indicates that strain decays to 15% of its original value in one second.
+        /// </summary>
+        protected abstract double StrainDecayBase { get; }
+
+        /// <summary>
+        /// <see cref="OsuDifficultyHitObject"/>s that were processed previously. They can affect the strain values of the following objects.
+        /// </summary>
+        protected readonly History<OsuDifficultyHitObject> Previous = new History<OsuDifficultyHitObject>(2); // Contained objects not used yet
+
+        private double currentStrain = 1; // We keep track of the strain level at all times throughout the beatmap.
+        private double currentSectionPeak = 1; // We also keep track of the peak strain level in the current section.
+        private readonly List<double> strainPeaks = new List<double>();
+        private readonly List<Tuple<OsuDifficultyHitObject, double>> objPeaks = new List<Tuple<OsuDifficultyHitObject, double>>();
+
+        /// <summary>
+        /// Process an <see cref="OsuDifficultyHitObject"/> and update current strain values accordingly.
+        /// </summary>
+        public void Process(OsuDifficultyHitObject current)
+        {
+            currentStrain *= strainDecay(current.DeltaTime);
+            if (!(current.BaseObject is Spinner))
+                currentStrain += StrainValueOf(current) * SkillMultiplier;
+
+            objPeaks.Add(new Tuple<OsuDifficultyHitObject, double>(current, currentStrain));
+
+            currentSectionPeak = Math.Max(currentStrain, currentSectionPeak);
+
+            Previous.Push(current);
+        }
+
+        /// <summary>
+        /// Saves the current peak strain level to the list of strain peaks, which will be used to calculate an overall difficulty.
+        /// </summary>
+        public void SaveCurrentPeak()
+        {
+            if (Previous.Count > 0)
+                strainPeaks.Add(currentSectionPeak);
+        }
+
+        /// <summary>
+        /// Sets the initial strain level for a new section.
+        /// </summary>
+        /// <param name="offset">The beginning of the new section in milliseconds</param>
+        public void StartNewSectionFrom(double offset)
+        {
+            // The maximum strain of the new section is not zero by default, strain decays as usual regardless of section boundaries.
+            // This means we need to capture the strain level at the beginning of the new section, and use that as the initial peak level.
+            if (Previous.Count > 0)
+                currentSectionPeak = currentStrain * strainDecay(offset - Previous[0].BaseObject.StartTime);
+        }
+
+        /// <summary>
+        /// Returns the calculated difficulty value representing all processed <see cref="OsuDifficultyHitObject"/>s.
+        /// </summary>
+        public double DifficultyValue()
+        {
+            strainPeaks.Sort((a, b) => b.CompareTo(a)); // Sort from highest to lowest strain.
+            objPeaks.Sort((a, b) => a.Item2.CompareTo(b.Item2)); // Sort from highest to lowest strain.
+
+            double difficulty = 0;
+            double weight = 1;
+
+            // Difficulty is the weighted sum of the highest strains from every section.
+            foreach (double strain in strainPeaks)
+            {
+                difficulty += strain * weight;
+                weight *= 0.9;
+            }
+
+            /*foreach  (Tuple<OsuDifficultyHitObject, double> obj in objPeaks) 
+            {
+                Console.WriteLine("---");
+                Console.WriteLine("Object placed: " + obj.Item1.BaseObject.StartTime);
+                Console.WriteLine("Strain value: " + obj.Item2);
+            }*/
+
+            return difficulty;
+        }
+
+        /// <summary>
+        /// Calculates the strain value of an <see cref="OsuDifficultyHitObject"/>. This value is affected by previously processed objects.
+        /// </summary>
+        protected abstract double StrainValueOf(OsuDifficultyHitObject current);
+
+        private double strainDecay(double ms) => Math.Pow(StrainDecayBase, ms / 1000);
+    }
+}
